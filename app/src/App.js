593cb4a63e4e00bfe2705fc86bf1a7d3f36df5e2
rzatalibov555 <sethubpro@gmail.com>
2024-02-19 17:06:52 +0400

    Create Components

---
+++ app/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import { Country } from './components/Country';
+import { Joker } from './components/Joker';
+
+class App extends React.Component{
+
+    render(){
+
+      const style = {
+        with: "100%",
+        minHeight: "400px",
+        background:"red"
+      }
+
+      return(
+        <div className='App' style={style}>
+          <h1 style={{color:"blue"}}>Hello</h1>
+          <Country></Country>
+          <Joker></Joker>
+        </div>
+      )
+    }
+
+}
+
+export default App;
---
+++ app/src/components/Country.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {Joker} from "./Joker"
+
+export const Country = () =>{
+    return(
+        <div>
+            <h2>Hi</h2>
+            <Joker></Joker>
+        </div>
+    )
+}
