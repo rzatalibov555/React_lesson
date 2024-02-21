babb31422184e8f1d2aa416657c349ec51547ddf
rzatalibov555 <sethubpro@gmail.com>
2024-02-21 14:29:07 +0400

    state function

--- app/src/App.js
+++ app/src/App.js
@@ -4,25 +4,36 @@ import './App.css';
 // import { Joker } from './components/Joker';
 import { Card } from './components/Cards';
 
-class App extends React.Component{
-
+class App extends React.Component {
+  
+  // https://react.dev/
   // state classlar ucundur. function componentler ucun ise hooklardan istifade olunur.
   state = {
-    animals:[{name: "cat", color: "pink"},
-              {name: "dog", color: "black"},
-              {name: "tiger", color: "orange"},
-              {name: "fox", color: "ginger"},
-              {name: "lion", color: "yellow"},
-            ]
-
-
+    animals: [{ name: "cat", color: "pink" },
+    { name: "dog", color: "black" },
+    { name: "tiger", color: "orange" },
+    { name: "fox", color: "ginger" },
+    { name: "lion", color: "yellow" },
+    ]
   }
-    render(){
-      // console.log(this.state)
 
-      let animal = this.state.animals
-      // console.log(animal)
-      return(
+  eventHandler() {
+      console.log("work")
+  }
+
+  eventHandler2(x) {
+    console.log(x)
+  }
+
+
+  render() {
+    
+
+
+    let animal = this.state.animals
+    // console.log(animal)
+    return (
+      <div>
         <div className='App'>
           <Card name={animal[0].name} color={animal[0].color} />
           <Card name={animal[1].name} color={animal[1].color} />
@@ -30,8 +41,22 @@ class App extends React.Component{
           <Card name={animal[3].name} color={animal[3].color} />
           <Card name={animal[4].name} color={animal[4].color} />
         </div>
-      )
-    }
+
+        <div>
+          {/* <button onClick={this.eventHandler}>push</button> */}
+          <button onClick={()=> this.eventHandler2('a')}>push</button>
+
+          <select onChange= {()=> this.eventHandler2('b')}>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+          </select>
+        </div>
+      </div>
+
+
+    )
+  }
 
 }
 
